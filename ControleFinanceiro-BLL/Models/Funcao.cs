e01f4ae4f2987db3e5584edb2b3478d2f6e2c361
Eduardo Alves Canuto <edutecni@gmail.com>
2022-04-29 16:43:45 -0300

    Casses e Maps Ok

---
+++ ControleFinanceiro-BLL/Models/Funcao.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleFinanceiro_BLL.Models
+{
+    public class Funcao : IdentityRole<string>
+    {
+        public string Descricao { get; set; }
+    }
+}
---
+++ ControleFinanceiro-DAL/Mappers/FuncaoMap.cs
@@ -0,0 +1,38 @@
+﻿using ControleFinanceiro_BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleFinanceiro_DAL.Mappers
+{
+    public class FuncaoMap : IEntityTypeConfiguration<Funcao>
+    {
+        public void Configure(EntityTypeBuilder<Funcao> builder)
+        {
+            builder.Property(f => f.Id).ValueGeneratedOnAdd();
+            builder.Property(f => f.Descricao).IsRequired().HasMaxLength(50);
+
+            builder.HasData(
+                    new Funcao
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Administrador",
+                        NormalizedName = "ADMINISTRADOR",
+                        Descricao = "Admnistrador do sistema"
+                    },
+
+                    new Funcao
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Usuario",
+                        NormalizedName = "USUARIO",
+                        Descricao = "Usuario do sistema"
+                    }
+                );
+
+            builder.ToTable("Funcoes");
+        }
+    }
+}
